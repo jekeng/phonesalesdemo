2d98b8adfed41d9cfe6e5a38feeba6aed86119de
jekeng <josephekeng98@gmail.com>
2025-04-20 20:06:14 -0400

    Update Phone_sales_final.py

--- Phone_sales_final.py
+++ Phone_sales_final.py
@@ -26,9 +26,11 @@ creds_dict = {
     "universe_domain": st.secrets["gcp"]["universe_domain"]
 }
 
+
+
 # Load the credentials from the dict and use them
 creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
-client = gspread.authorize(creds_dict)
+client = gspread.authorize(creds)
 
 # Open the Google Sheet by name
 sheet = client.open("Phone Sales Tracker").sheet1  # Replace with your sheet name
